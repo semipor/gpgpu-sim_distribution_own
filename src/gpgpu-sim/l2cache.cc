cc297e7807af7d7675deec0e1bbcf22b685b3a84
Hyunkyun Shin <hkshin@dgist.ac.kr>
2024-05-19 19:13:57 +0900

    Update l2cache.cc

--- src/gpgpu-sim/l2cache.cc
+++ src/gpgpu-sim/l2cache.cc
@@ -497,14 +497,14 @@ void memory_sub_partition::cache_cycle(unsigned cycle) {
     //printf("%llu, %u\n",m_gpu->gpu_sim_cycle + m_gpu->gpu_tot_sim_cycle, cycle);
     //schedule decompression per memory controller.
     //2060 : 12 memctrl per GPU, 2 subpartition per 1 memctrl.
-    /*
-    if(m_decomp_q->size() < 12){
+    
+    if(m_decomp_q->size() < 30){
       mf->decomp_cycle = m_gpu->gpu_sim_cycle + m_gpu->gpu_tot_sim_cycle;
       m_decomp_q->push(mf);
       m_dram_L2_queue->pop();
       printf("%u, %u\n",cycle, m_decomp_q->front()->decomp_cycle);
     }
-    */
+    /*
     if (!m_config->m_L2_config.disabled() && m_L2cache->waiting_for_fill(mf)) {
       if (m_L2cache->fill_port_free()) {
         mf->set_status(IN_PARTITION_L2_FILL_QUEUE,
@@ -512,19 +512,17 @@ void memory_sub_partition::cache_cycle(unsigned cycle) {
         m_L2cache->fill(mf, m_gpu->gpu_sim_cycle + m_gpu->gpu_tot_sim_cycle +
                                 m_memcpy_cycle_offset);
         m_dram_L2_queue->pop();
-        printf("%u\n",cycle);
       }
     } else if (!m_L2_icnt_queue->full()) {
       if (mf->is_write() && mf->get_type() == WRITE_ACK)
         mf->set_status(IN_PARTITION_L2_TO_ICNT_QUEUE,
                        m_gpu->gpu_sim_cycle + m_gpu->gpu_tot_sim_cycle);
       m_L2_icnt_queue->push(mf);
-      printf("%u\n",cycle);
       m_dram_L2_queue->pop();
     }
+    */
   }
 
-  /*
   if(m_decomp_q->size() > 0 && m_decomp_q->front()->decomp_cycle + 11 < cycle) {
     printf("cur_size : %u\n", m_decomp_q->size());
     mem_fetch *mf = m_decomp_q->front();
@@ -544,7 +542,6 @@ void memory_sub_partition::cache_cycle(unsigned cycle) {
         m_decomp_q->pop();
     }
   }
-  */
   
 
   // prior L2 misses inserted into m_L2_dram_queue here
